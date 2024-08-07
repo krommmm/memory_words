eb1e4c67265b98fb629e02669072a24261f03be5
Thibault Boutaud <krom@gmx.fr>
2024-08-07 16:18:52 +0200

    ajout all

--- js/class/utils/Sounds.js
+++ js/class/utils/Sounds.js
@@ -6,29 +6,29 @@ export class Sounds extends ISounds {
     }
 
     success() {
-        const audio = new Audio();
-        audio.src = "assets/files/success_sound.wav";
-        audio.volume = 0.1; 
-        audio.play();
+        // const audio = new Audio();
+        // audio.src = "assets/files/success_sound.wav";
+        // audio.volume = 0.1; 
+        // audio.play();
     }
 
     faill() {
-        const audio = new Audio();
-        const denis = "assets/files/denis_nedry.mp3";
-        const chewbacca = "assets/files/chewbacca.swf.mp3";
-        audio.src = denis;
-        audio.volume = 0.1; 
-        audio.play();
+        // const audio = new Audio();
+        // const denis = "assets/files/denis_nedry.mp3";
+        // const chewbacca = "assets/files/chewbacca.swf.mp3";
+        // audio.src = denis;
+        // audio.volume = 0.1; 
+        // audio.play();
     }
 
     pass() {
-        const heartBeat = "https://universal-soundbank.com/sounds/350.mp3";
-        const magneto = "https://universal-soundbank.com/sounds/3802.mp3";
-        const clacquement = "https://universal-soundbank.com/sounds/2166.mp3";
-        const audio = new Audio();
-        audio.src = clacquement;
-        audio.volume = 0.1; 
-        audio.play();
+        // const heartBeat = "https://universal-soundbank.com/sounds/350.mp3";
+        // const magneto = "https://universal-soundbank.com/sounds/3802.mp3";
+        // const clacquement = "https://universal-soundbank.com/sounds/2166.mp3";
+        // const audio = new Audio();
+        // audio.src = clacquement;
+        // audio.volume = 0.1; 
+        // audio.play();
     }
 
     speak(text) {
